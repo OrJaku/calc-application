43a3094b5c7bf33a5518a9e384fd677c5e9f6914
OrJaku <kuba.orzel@hotmail.com>
2020-02-24 22:54:00 +0100

    Implementing models

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from keras.models import load_model, model_from_json
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
+
+data_path = os.path.join(basedir, 'data')
+
+with open('data_path/model_config.json', 'r') as f:
+    model = model_from_json(f.read())
+
+model.load_weights('model_mnist.h5')
 
 db = SQLAlchemy()
 
